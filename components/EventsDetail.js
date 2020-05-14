17d52532d86d6a28b9d78cced70b5e2181dfeb18
akinayturan <akinayturan@gmail.com>
2020-05-14 03:11:25 +0300

    edt

--- components/EventsDetail.js
+++ components/EventsDetail.js
@@ -2,88 +2,79 @@ import React from 'react';
 import Map from "./Map";
 
 const EventsDetail = () => {
-    return (
-        <section className="event-detail-area">
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-12">
-                        <div className="blog-content">
-                            <div className="blog-item">
-                                <div className="blog-img">
-                                    <img src="/images/img19.jpg" alt="" />
-                            <span className="blog__tag">
-                                <span className="date__num-text">9</span>
-                                <span className="date__mon-text">mar</span>
-                            </span>
+    return (<section className="causes-detail-area news-detail-area">
+                <div className="container">
+                    <div className="row blog-content-wrap">
+                        <div className="col-lg-12">
+                            <h3 className="blog__title"><a href="#">A COMPARATIVE STUDY TO ENHANCE THE PHOTOCATALYTIC ACTIVITY OF  PICKERING EMULSIONS STABILIZED WITH DIFFERENT SURFACE-MODIFIED TITANIUM DIOXIDES  VIA MEMBRANE EMULSIFICATION WAY</a>
+                            </h3>
+                        </div>
+                        <div className="col-lg-8">
+                            <div className="blog-content">
+                                <div className="blog-item">
+                                    <div className="blog-inner-content">
+                                        <div className="inner-causes-box">
+
+                                            <ul className="blog__list">
+                                                <li><a href="#">3 Author(s):</a></li>
+                                                <li className="blog__dot-active"><a href="#">asd ad</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div className="blog-inner-content-2">
+                                        <p className="causes__text">
+                                        <strong>
+                                            Abstract:
+                                        </strong>   asdasdasdasddsa
+
+                                        </p>
+
+                                        <div className="news-tags">
+                                            <div className="news-tag-item-left">
+                                    <span className="news-meta-tags">
+                                        <span className="news-meta-title">Keywords</span>
+                                        <a href="#">pickering emulsion</a>
+                                        <a href="#">surface modification</a>
+                                        <a href="#">Dye Decolorization</a>
+                                    </span>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4">
+                            <div className="sidebar-shared">
+                                <div className="side-widget">
+                                    <h2 className="widget__title">Presentation Type: Oral</h2>
+                                    <div className="author-box recent-donate-item">
+                                        <a className="reply__btn theme-btn" href="#">PDF</a>
+                                    </div>
+                                </div>
+                                <div className="side-widget">
+                                    <h2 className="widget__title">Cite This (APA)
+                                    </h2>
+                                    <div className="side-cats">
+                                        A. Orsdemir, H. O. Altun, G. Sharma and M. F. Bocko, "On the security and robustness of encryption via compressed sensing," MILCOM 2008 - 2008 IEEE Military Communications Conference, San Diego, CA, 2008, pp. 1-7, doi: 10.1109/MILCOM.2008.4753187.
+
+                                    </div>
+                                </div>
+                                <div className="side-widget">
+                                    <h2 className="widget__title">Other Cites</h2>
+                                    <div className="side-tags">
+                                        <ul>
+                                            <li><a href="#">BibTeX</a></li>
+                                            <li><a href="#">RIS</a></li>
+                                            <li><a href="#">EndNote</a></li>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-8">
-                        <div className="event-detail-content">
-                            <div className="event-detail-item">
-                                <h3 className="event__title">Play for the world</h3>
-                                <p className="event__text">
-                                    There are many variations of passages of Lorem Ipsum available,
-                                    but the majority have suffered alteration in some form, by injected humour,
-                                    or randomised words which don't look even slightly believable.
-                                    If you are going to use a passage of Lorem Ipsum,
-                                    you need to be sure there isn't anything embarrassing hidden in the middle of text.
-                                </p>
-                                <p className="event__text">
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                                    when an unknown printer took a galley of type and scrambled it to make a type
-                                    specimen book.
-                                    It has survived not only five centuries, but also the leap into electronic
-                                    typesetting, remaining essentially unchanged. It was popularised in the 1960s with
-                                    the release of Letraset sheets containing Lorem Ipsum passages,
-                                    and more recently with desktop publishing software like Aldus PageMaker including
-                                    versions
-                                    of Lorem Ipsum ley of type and scrambled it to make a type specimen book.
-                                </p>
-                            </div>
-                            <div className="event-detail-item">
-                                <h3 className="event__title event__title2">Event Requirements</h3>
-                                <p className="event__text">
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                                    when an unknown printer took a galley of type and scrambled it to make a type
-                                    specimen book.
-                                    It has survived not only five centuries, but also the leap into electronic
-                                    typesetting, remaining essentially unchanged. It was popularised in the 1960s with
-                                    the release of Letraset sheets containing Lorem Ipsum passages,
-                                    and more recently with desktop publishing software like Aldus PageMaker including
-                                    versions
-                                    of Lorem Ipsum ley of type and scrambled it to make a type specimen book.
-                                </p>
-                            </div>
-                            <div className="event-detail-btn">
-                                <a href="#" className="theme-btn">register now</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4">
-                        <div className="event-detail-sidebar">
-                            <div className="event-detail-item">
-                                <h3 className="event__title">Event Details</h3>
-                                <ul className="event__list">
-                                    <li><span>Starting Time:</span> 8:00AM to 2:00PM</li>
-                                    <li><span>Date:</span>9 March, 2019</li>
-                                    <li><span>Category:</span>Health</li>
-                                    <li><span>Phone:</span>666 888 0000</li>
-                                    <li><span>Website:</span>Info@event.com</li>
-                                    <li><span>Location:</span>8 Street, San Marcos London D29, UK</li>
-                                </ul>
-                            </div>
-                            <div className="event-detail-item event-detail-item2">
-                                <Map />
-                            </div>
-                        </div>
-                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
     );
 };
 
