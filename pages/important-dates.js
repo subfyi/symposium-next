7f1541e051345aa9aabfbbfcaa403b283a78acd9
akinayturan <akinayturan@gmail.com>
2020-07-02 14:39:04 +0300

    edt 2

--- pages/important-dates.js
+++ pages/important-dates.js
@@ -2,13 +2,7 @@ import React from 'react';
 import Layout from "../components/Layout";
 import NavOne from "../components/NavOne";
 import PageHeader from "../components/PageHeader";
-import AboutArea from "../components/AboutArea";
 import MixerAreaTwo from "../components/MixerAreaTwo";
-import CallToAction from "../components/CallToAction";
-import TeamArea from "../components/TeamArea";
-import FaqArea from "../components/FaqArea";
-import ServiceArea from "../components/ServiceArea";
-import ClientsLogoTwo from "../components/ClientsLogoTwo";
 import Footer from "../components/Footer";
 
 const AboutPage = () => {
