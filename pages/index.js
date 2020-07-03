a5cbccb118a5391c1be99ab69abd2bf3a721e764
akinayturan <akinayturan@gmail.com>
2020-07-03 21:36:39 +0300

    e1

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,25 @@ import CallToAction from "../components/CallToAction";
 const HomePage = () => {
     return (
         <Layout pageTitle="ISESER2020 - International Symposium for Environmental Science and Engineering Research">
-          <NavOne />
-            <CallToAction />
-          <SliderOne />
-          <ServiceArea />
-          <Footer />
+            <NavOne/>
+            <CallToAction/>
+            <section className="service-area text-center">
+                <div className="container">
+
+                    <div className="row service-wrap">
+                        <div className="col">
+                           Welcome to ISESER 2020. All presentations can be watched online from the <b><a href="https://portal.iseser.com/"><i className="fa fa-external-link"></i> user area</a></b> after you login (If you do not have an account, please create a new account).
+                                You can make
+                                your comments under any presentation in "Questions & Answers" section by clicking "Add New" button.
+
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+            <SliderOne/>
+            <ServiceArea/>
+            <Footer/>
         </Layout>
     );
 };
