5e0a844e5f2a3c39184e07ddc8884e88932086b0
akinayturan <akinayturan@gmail.com>
2020-08-11 16:00:22 +0300

    e

--- api.js
+++ api.js
@@ -4,7 +4,7 @@ export default async (url, params) => {
     var newParams = params && { ...params, headers: { 'Accept': 'application/json', ...(params.headers || {}) } };
 
     if (url.startsWith('/')) {
-        task = fetch("https://api.iseser.com/" + url, newParams);
+        task = fetch("https://api.iseser.com" + url, newParams);
     } else {
         task = fetch(url, newParams);
     }
