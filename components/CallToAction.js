de224226bf0079784a82952f3b88ab662cd346ac
akinayturan <akinayturan@gmail.com>
2021-03-18 20:45:20 +0300

    2021 e6

--- components/CallToAction.js
+++ components/CallToAction.js
@@ -5,17 +5,15 @@ const CallToAction = () => {
         <section className="mixer-area mixer-area3">
             <div className="container">
                 <div className="row">
-
-                    <div className="col-lg-2"></div>
-                    <div className="col-lg-2">
+                    <div className="col-lg-1"></div>
+                    <div className="col-lg-3">
                         <div className="section-heading mixer-heading">
                             <a href="/documents" className="theme-btn">Documents</a>
                         </div>
                     </div>
                     <div className="col-lg-8">
                         <div className="section-heading mixer-heading">
-                            <p className="section-heading-p">
-                                <a target="_blank" href="/doc/2021/ISESER2021_BANNER.pdf?v1" className="theme-btn">Click for 2021 Banner</a></p>
+                                <a target="_blank" href="/doc/2021/ISESER2021_BANNER.pdf?v1" className="theme-btn">Click for 2021 Banner</a>
                         </div>
                     </div>
                 </div>
