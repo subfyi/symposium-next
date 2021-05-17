acc90695e2920240bd4d5aa0f0682d61983d6aef
akinayturan <akinayturan@gmail.com>
2021-05-17 11:00:37 +0300

    edt 5

--- pages/sponsors.js
+++ pages/sponsors.js
@@ -30,11 +30,17 @@ const AboutPage = () => {
                                     <p className="section__desc">
                                         <ul>
 
-                                            <li>Journal of International Environmental Application &amp; Science (JIEAS), <a target="_blank" href="http://www.jieas.com">jieas.com</a>
+                                            <li>
+                                                Journal of International Environmental Application &amp; Science (JIEAS), <a target="_blank" href="http://www.jieas.com">jieas.com</a>
                                             </li>
-                                            <li>International Journal of Environmental Pollution and Environmental Modelling (IJEPEM), <a target="_blank" href="https://ijepem.com">ijepem.com</a>
+                                            <li>
+                                                Journal of Research in Atmospheric Science (JRAS), <a target="_blank" href="https://resatmsci.com">resatmsci.com</a>
                                             </li>
-                                            <li>Gnosi: An Interdisciplinary Journal of Human Theory and Praxis, <a target="_blank" href="https://gnosijournal.com">gnosijournal.com</a>
+                                            <li>
+                                                International Journal of Environmental Pollution and Environmental Modelling (IJEPEM), <a target="_blank" href="https://ijepem.com">ijepem.com</a>
+                                            </li>
+                                            <li>
+                                                Gnosi: An Interdisciplinary Journal of Human Theory and Praxis, <a target="_blank" href="https://gnosijournal.com">gnosijournal.com</a>
                                             </li>
                                         </ul>
                                     </p>
