09412edd3d5dbd1d2f43f7b214624d802147ccba
akinayturan <akinayturan@gmail.com>
2023-06-14 23:35:12 +0300

    2023 e.1.6.2 Analytics test

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import 'swiper/css/swiper.css';
-import { Analytics } from '@vercel/analytics/react';
-
-export default function MyApp({ Component, pageProps }) {
-    <>
-        <Component {...pageProps} />
-        <Analytics />
-    </>
-}
