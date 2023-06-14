9e89bbc8374654de4b7eb26fa113b4906c19f046
akinayturan <akinayturan@gmail.com>
2023-06-14 23:31:30 +0300

    2023 e.1.6 Analytics test

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import 'swiper/css/swiper.css';
+import { Analytics } from '@vercel/analytics/react';
 
-// This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    <>
+        <Component {...pageProps} />
+        <Analytics />
+    </>
 }
