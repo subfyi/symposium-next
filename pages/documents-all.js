062d4ec435e2848a5de036464a21ccd77f61ff1a
akinayturan <akinayturan@gmail.com>
2023-10-13 10:46:11 +0300

    2023 edts-1.34

--- pages/documents-all.js
+++ pages/documents-all.js
@@ -30,6 +30,7 @@ export default function DocumentsAll() {
                   <div className='section__desc'>
                     <ul className='icon'>
                       <li className='star'><a href='https://api.iseser.com/doc/2023/ISESER2023_BANNER.pdf?v5' target='_blank'><i className='fa  fa-file-pdf-o'></i> BANNER</a></li>
+                      <li className='star'><a href='../doc/2023/ISESER2023-PROGRAM-PRELIMINARY.pdf?e5' target='_blank' rel='noopener noreferrer'><i className='fa  fa-file-pdf-o'></i> PRELIMINARY PROGRAM</a></li>
                       <li className='star'>
                         <a href='../year/2023'><i className='fa fa-file-code-o'></i> WEB VERSION (ONLINE VIEW)</a>
                       </li>
