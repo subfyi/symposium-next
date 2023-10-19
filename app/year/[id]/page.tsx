b3c7c5a6136430b7ee339d294a2d925ed209f0b2
akinayturan <akinayturan@gmail.com>
2023-10-19 12:29:13 +0300

    2023 e1.2

--- app/year/[id]/page.tsx
+++ app/year/[id]/page.tsx
@@ -1,32 +1,32 @@
-import api from "@/api";
-import YearPageClient from "@/app/year/[id]/client";
+import api from '@/api'
+import YearPageClient from '@/app/year/[id]/client'
 
 export function generateMetadata(
-    {params, searchParams}:
-        {
-            params: { id: string }; searchParams?:
-                { [key: string]: string | string[] | undefined };
-        }
+  { params, searchParams }:
+    {
+      params: { id: string }; searchParams?:
+        { [key: string]: string | string[] | undefined };
+    }
 ) {
 
-    return {
-        title: params.id + " | ISESER",
+  return {
+    title: 'ISESER' + params.id + ' ABSTRACTS ONLINE VIEW'
 
-    };
+  }
 }
 
 export default async function Page(
-    {params, searchParams}:
-        {
-            params: { id: string }; searchParams?:
-                { [key: string]: string | string[] | undefined };
-        }
+  { params, searchParams }:
+    {
+      params: { id: string }; searchParams?:
+        { [key: string]: string | string[] | undefined };
+    }
 ) {
 
-    var [orals, posters] = await Promise.all([
-        api('/api/submission?page=1&itemPerPage=-1&query=&sort=id&desc=false&orals=1&year=' + params?.id),
-        api('/api/submission?page=1&itemPerPage=-1&query=&sort=id&desc=false&posters=1&year=' + params?.id)
-    ])
+  var [orals, posters] = await Promise.all([
+    api('/api/submission?page=1&itemPerPage=-1&query=&sort=id&desc=false&orals=1&year=' + params?.id),
+    api('/api/submission?page=1&itemPerPage=-1&query=&sort=id&desc=false&posters=1&year=' + params?.id)
+  ])
 
-    return <YearPageClient orals={orals} posters={posters} year={params?.id}/>;
+  return <YearPageClient orals={orals} posters={posters} year={params?.id} />
 }
