1c1a9db24b64f0735b474b2b7da76af6ae99e4a0
akinayturan <akinayturan@gmail.com>
2023-10-24 20:52:29 +0300

    Update Full Text Deadline on important-dates page
    The Full Text Deadline date has been changed from 18th October 2023 to 30th October 2023. This update was necessary to reflect the recent changes in the schedule of the event.

--- app/important-dates/page.tsx
+++ app/important-dates/page.tsx
@@ -23,7 +23,7 @@ export default function Page() {
                                 <p className='section__desc'>
                                     <ul>
                                         <li>Registration and Abstract Submission Date: 1 October 2023</li>
-                                        <li>Full Text Deadline: 18 October 2023</li>
+                                        <li>Full Text Deadline: 30 October 2023</li>
                                         <li>Symposium Date: 19-21 October 2023</li>
 
                                     </ul>
