f5f72afbbbeed21d22e1511732f21590bc9e3877
akinayturan <akinayturan@gmail.com>
2023-10-29 19:14:26 +0300

    Update document component to support dynamic year selection
    This commit refactors the `page.tsx` implementation, specifically replacing the static `Document2023` component with a flexible `DocumentWithYear` component that accepts a year as its prop. The intent is to streamline the reference to documents of different years, simplifying the process of adding and managing documents related to each subsequent year. This change emphasizes maintainability and scalability of our document management.

--- app/documents/page.tsx
+++ app/documents/page.tsx
@@ -1,7 +1,7 @@
 import { PageHeader } from '@/layout/Breadcrumb'
 import Announcement from '@/components/Announcement'
 import Enlightenment from '@/components/Enlightenment'
-import { Document2023 } from '@/app/documents-all/page'
+import { Document2023, DocumentWithYear } from '@/app/documents-all/page'
 
 const metaTitle = 'Documents'
 export const metadata = {
@@ -30,7 +30,11 @@ export default function Page() {
                 <ul className='icon'>
                   <li><a href='https://portal.iseser.com/'><i className='fa fa-external-link'></i> REGISTRATION</a></li>
                 </ul>
-                <Document2023 />
+                <DocumentWithYear year={2023} />
+                <ul>
+                  <li><a href='https://photos.app.goo.gl/ftS6XjNtDY91a1rT7' target='_blank' rel='noopener noreferrer'><i className='fa-regular fa-file-image' aria-hidden='true'></i> PHOTOS </a></li>
+                  <li><a href='https://www.youtube.com/playlist?list=PLcT5bWlEGP-2fKrCEWPUJ4vDa7Rd0e4n1' target='_blank' rel='noopener noreferrer'><i className='fa-regular fa-file-video' aria-hidden='true'></i> VIDEOS </a></li>
+                </ul>
                 <hr />
                 <ul>
                   <li><a href='https://api.iseser.com/doc/2023/ISESER2023_Abstract_Template.docx' target='_blank'><i className='fa-regular fa-file-word'></i> Abstract Template</a>
