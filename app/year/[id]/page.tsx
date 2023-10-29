363e5a1ecc4e992ee1ee0d41d35874112fde5da6
akinayturan <akinayturan@gmail.com>
2023-10-29 19:27:14 +0300

    Add dynamic SEO description for symposium and paper pages
    In this commit, 'description' fields were added to both the symposium and paper page components. These descriptions provide more detailed metadata about each page for improving the Search Engine Optimization (SEO). It will enhance the discoverability of each specific symposium and the respective papers presented during that event in search engine results.

--- app/year/[id]/page.tsx
+++ app/year/[id]/page.tsx
@@ -13,8 +13,10 @@ export function generateMetadata(
 
   return {
     title: 'ISESER' + params.id.toUpperCase() + ' ABSTRACTS ONLINE VIEW',
+    description: 'ISESER' + params.id.toUpperCase() + ' Symposium Abstracts Online Version" is a digital repository that features a compilation of abstracts related to the ISESER' + params.id.toUpperCase() + ' Symposium.',
     openGraph: {
-      title: 'ISESER' + params.id.toUpperCase() + ' ABSTRACTS ONLINE VIEW'
+      title: 'ISESER' + params.id.toUpperCase() + ' ABSTRACTS ONLINE VIEW',
+      description: 'ISESER' + params.id.toUpperCase() + ' Symposium Abstracts Online Version" is a digital repository that features a compilation of abstracts related to the ISESER' + params.id.toUpperCase() + ' Symposium.',
     }
   }
 }
